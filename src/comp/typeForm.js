43fdcb9eb3a17e2c9b2e445772ac4aea0f7de81d
dr0id007 <nullvoid.nv@gmail.com>
2019-10-16 21:01:05 +0530

    initial commit

---
+++ src/comp/typeForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+
+export default class App extends React.Component {
+  render() {
+    return <ReactTypeformEmbed url="https://dev772537.typeform.com/to/fIpvdd" />;
+  }
+}
